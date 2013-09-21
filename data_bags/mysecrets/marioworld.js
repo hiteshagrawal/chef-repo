813263c0e1be32b614211af9ae5b91dd0362dca1
Hitesh Agrawal <hitesh_a2003@yahoo.com>
2013-09-21 21:00:53 +0800

    ok , adding encrypted data bags

--- data_bags/mysecrets/marioworld.js
+++ data_bags/mysecrets/marioworld.js
@@ -1,14 +1,14 @@
 {
   "id": "marioworld",
   "user": {
-    "encrypted_data": "5jpeGXfKa8YalOF2CqMSKHyTvYCIX0HJq7jFMeqzWSg=\n",
-    "iv": "zpHzhZEnlbejOsiwNUIy0Q==\n",
+    "encrypted_data": "meEWUDOS/59T2+9zROGtWbcyc+Ks4WBjhrbheDzCF70=\n",
+    "iv": "3HBXBw8DmNbnGbyE3rfj+A==\n",
     "version": 1,
     "cipher": "aes-256-cbc"
   },
   "pass": {
-    "encrypted_data": "V199AWJ1B8oaublVOAemyWqnQhz8n/ilaZRLExAuZa4=\n",
-    "iv": "OnLNo5y5zunwmfnrcRkhnA==\n",
+    "encrypted_data": "TMJRMyfl4pgJzQmgdsORdu9/JMVpxLrip573DsVdc/M=\n",
+    "iv": "ztlTQgOVtODQgcIVVV994g==\n",
     "version": 1,
     "cipher": "aes-256-cbc"
   }

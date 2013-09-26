428b916795a235bd0926c49fb231715716154803
Hitesh Agrawal <hitesh_a2003@yahoo.com>
2013-09-26 16:35:06 +0800

    Adding abc.js

---
+++ data_bags/mysecrets/abc.js
@@ -0,0 +1,15 @@
+{
+  "id": "abc",
+  "user": {
+    "encrypted_data": "meEWUDOS/59T2+9zROGtWbcyc+Ks4WBjhrbheDzCF70=\n",
+    "iv": "3HBXBw8DmNbnGbyE3rfj+A==\n",
+    "version": 1,
+    "cipher": "aes-256-cbc"
+  },
+  "pass": {
+    "encrypted_data": "TMJRMyfl4pgJzQmgdsORdu9/JMVpxLrip573DsVdc/M=\n",
+    "iv": "ztlTQgOVtODQgcIVVV994g==\n",
+    "version": 1,
+    "cipher": "aes-256-cbc"
+  }
+}
